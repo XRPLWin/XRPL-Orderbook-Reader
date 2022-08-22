119ed83a45106c5843633ee05c8d5ca53a232e29
ZGrguric <zgrguric@xrplwin.com>
2022-08-22 23:56:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,65 @@
-# XRPL-Orderbook-Reader
-Parse XRPL Order Book results into effective liquidity based exchange prices
+![main workflow](https://github.com/XRPLWin/XRPL-Orderbook-Reader/actions/workflows/main.yml/badge.svg)
+[![GitHub license](https://img.shields.io/github/license/XRPLWin/XRPL-Orderbook-Reader)](https://github.com/XRPLWin/XRPL-Orderbook-Reader/blob/main/LICENSE)
+[![Total Downloads](https://img.shields.io/packagist/dt/xrplwin/xrpl-orderbook-reader.svg?style=flat)](https://packagist.org/packages/xrplwin/xrpl-orderbook-reader)
 
-This is PHP port of https://github.com/XRPL-Labs/XRPL-Orderbook-Reader
+# XRPL Orderbook Reader for PHP
 
+This is PHP port of https://github.com/XRPL-Labs/XRPL-Orderbook-Reader by [Wietse Wind](https://github.com/WietseWind) ([@XRPL Labs](https://github.com/XRPL-Labs))
+
+This repository takes XRPL Orderbook (`book_offers`) datasets and requested volume to
+exchange and calculates the effective exchange rates based on the requested and available liquidity.
+
+Optionally certain checks can be specified (eg. `book_offers` on the other side of the book)
+to warn for limited (percentage) liquidity on the requested side, and possibly other side
+of the order book.
+
+## Requirements
+- PHP 8.1 or higher
+- [Composer](https://getcomposer.org/)
+
+## Installation
+This Package is still in **beta**, to install run
+
+```
+composer require xrplwin/xrpl-orderbook-reader
+```
+
+## Usage
+```PHP
+use \XRPLWin\XRPL\Client;
+
+$xrplwinapiclient = new Client([]);
+$lc = new LiquidityCheck([
+    # Trade:
+    'from' => [
+        'currency' => 'XRP'
+    ],
+    'to' => [
+        'currency' => 'USD',
+        'issuer' => 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq'
+    ],
+    'amount' => 10,
+    'limit' => 100
+],
+[
+    # Options:
+    //'rates' => 'to',
+    //'maxSpreadPercentage' => 4
+    //'maxSlippagePercentage' => 3
+    //'maxSlippagePercentageReverse' => 3
+    'includeBookData' => true //default false
+], $xrplwinapiclient);
+
+$Liquidity = $lc->get();
+
+print_r($Liquidity);
+```
+## Running tests
+Run all tests in "tests" directory.
+```
+composer test
+```
+or
+```
+./vendor/bin/phpunit --testdox
+```
---
+++ tests/ReaderTest.php
@@ -0,0 +1,138 @@
+<?php declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+#use GuzzleHttp\Handler\MockHandler;
+#use GuzzleHttp\HandlerStack;
+#use GuzzleHttp\Psr7\Response;
+#use XRPLWin\XRPL\Client\Guzzle\HttpClient;
+use \XRPLWin\XRPL\Client;
+use \XRPLWin\XRPLOrderbookReader\LiquidityCheck;
+
+final class ReaderTest extends TestCase
+{
+    public function testShouldGetRateForXrpFromGatehubUsd(): void
+    {
+        $client = new Client([
+            'endpoint_reporting_uri' => 'https://xrplcluster.com'
+        ]);
+
+        $lc = new LiquidityCheck([
+            # Trade:
+            'from' => [
+                'currency' => 'XRP'
+            ],
+            'to' => [
+                'currency' => 'USD',
+                'issuer' => 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq'
+            ],
+            'amount' => 20,
+            'limit' => 500
+        ],
+        [
+            # Options:
+            //'rates' => 'to',
+            //'maxSpreadPercentage' => 4,
+            //'maxSlippagePercentage' => 3,
+            //'maxSlippagePercentageReverse' => 3,
+            'includeBookData' => true
+        ],
+        $client);
+
+        $Liquidity = $lc->get();
+
+        $this->assertIsArray($Liquidity);
+        $this->assertEquals(4,count($Liquidity));
+        $this->assertArrayHasKey('rate',$Liquidity);
+        $this->assertIsNumeric($Liquidity['rate']);
+
+        $this->assertArrayHasKey('safe',$Liquidity);
+        $this->assertIsBool($Liquidity['safe']);
+
+        $this->assertArrayHasKey('errors',$Liquidity);
+        $this->assertIsArray($Liquidity['errors']);
+
+        $this->assertArrayHasKey('books',$Liquidity);
+        $this->assertIsArray($Liquidity['books']);
+        $this->assertEquals(2,count($Liquidity['books']));
+        
+        $this->assertGreaterThan(0,$Liquidity['rate']);
+    }
+
+    public function testShouldExceedAbsurdLimitsForXrpToGatehubUsd()
+    {
+        $client = new Client([
+            'endpoint_reporting_uri' => 'https://xrplcluster.com'
+        ]);
+
+        $lc = new LiquidityCheck([
+            # Trade:
+            'from' => [
+                'currency' => 'XRP'
+            ],
+            'amount' => 100000,
+            'to' => [
+                'currency' => 'USD',
+                'issuer' => 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq'
+            ],
+            'limit' => 100
+        ],
+        [
+            # Options:
+            'rates' => 'to',
+            'maxSpreadPercentage' => 0.0001,
+            'maxSlippagePercentage' => 0.0001,
+            'maxSlippagePercentageReverse' => 0.0001,
+            'includeBookData' => false
+        ],
+        $client);
+
+        $Liquidity = $lc->get();
+        //print_r($Liquidity['errors']);
+        $this->assertEquals([
+            //'REQUESTED_LIQUIDITY_NOT_AVAILABLE',
+            //'REVERSE_LIQUIDITY_NOT_AVAILABLE',
+            'MAX_SPREAD_EXCEEDED',
+            'MAX_SLIPPAGE_EXCEEDED',
+            'MAX_REVERSE_SLIPPAGE_EXCEEDED'
+        ],$Liquidity['errors']);
+    }
+
+    public function testShouldErrorOutWithInsufficientLiquidity()
+    {
+        $client = new Client([
+            'endpoint_reporting_uri' => 'https://xrplcluster.com'
+        ]);
+
+        $lc = new LiquidityCheck([
+            # Trade:
+            'from' => [
+                'currency' => 'XRP'
+            ],
+            'amount' => 1000,
+            'to' => [
+                'currency' => 'USD',
+                'issuer' => 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq'
+            ],
+            'limit' => 100
+        ],
+        [
+            # Options:
+            'rates' => 'to',
+            'maxSpreadPercentage' => 2,
+            'maxSlippagePercentage' => 2,
+            'maxSlippagePercentageReverse' => 2,
+            'includeBookData' => false
+        ],
+        $client);
+
+        $Liquidity = $lc->get();
+
+        $this->assertEquals([
+            'REQUESTED_LIQUIDITY_NOT_AVAILABLE',
+            'REVERSE_LIQUIDITY_NOT_AVAILABLE',
+            'MAX_SPREAD_EXCEEDED'
+        ],$Liquidity['errors']);
+    }
+}
